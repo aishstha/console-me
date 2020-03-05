7bcfc0dcd6e8ed6916a34a5abf54aee96c793bba
Aishwarya <asworeya2shrestha@gmail.com>
2020-03-06 00:54:14 +0545

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+### Control
+
+Insert console : shift+ctrl+b
+Delete console: shift+ctrl+d
---
+++ extension.js
@@ -0,0 +1,91 @@
+const vscode = require('vscode');
+
+const insertText = (val) => {
+    const editor = vscode.window.activeTextEditor;
+
+    if (!editor) {
+        vscode.window.showErrorMessage('Can\'t insert log because no document is open');
+        return;
+    }
+
+    const selection = editor.selection;
+    const range = new vscode.Range(selection.start, selection.end);
+
+    editor.edit((editBuilder) => {
+        editBuilder.replace(range, val);
+    });
+}
+
+function getAllLogStatements(document, documentText) {
+    let logStatements = [];
+
+    const logRegex = /console.(log|debug|info|warn|error|assert|dir|dirxml|trace|group|groupEnd|time|timeEnd|profile|profileEnd|count)\((.*)\);?/g;
+    let match;
+    while (match = logRegex.exec(documentText)) {
+        let matchRange =
+            new vscode.Range(
+                document.positionAt(match.index),
+                document.positionAt(match.index + match[0].length)
+            );
+        if (!matchRange.isEmpty)
+            logStatements.push(matchRange);
+    }
+    return logStatements;
+}
+
+function deleteFoundLogStatements(workspaceEdit, docUri, logs) {
+    logs.forEach((log) => {
+        workspaceEdit.delete(docUri, log);
+    });
+
+    vscode.workspace.applyEdit(workspaceEdit).then(() => {
+        logs.length > 1
+            ? vscode.window.showInformationMessage(`${logs.length} console.logs deleted`)
+            : vscode.window.showInformationMessage(`${logs.length} console.log deleted`);
+    });
+}
+
+function activate(context) {
+    const insertLogStatement = vscode.commands.registerCommand('extension.insertConsole', () => {
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) { return; }
+
+		const selection = editor.selection;
+		
+        const text = editor.document.getText(selection);
+
+        text
+            ? vscode.commands.executeCommand('editor.action.insertLineAfter')
+                .then(() => {
+                    const logToInsert = `console.log('${text}: ', ${text});`;
+                    insertText(logToInsert);
+                })
+			: insertText('console.log();')
+			;
+	});
+	
+    context.subscriptions.push(insertLogStatement);
+
+    const deleteAllLogStatements = vscode.commands.registerCommand('extension.deleteAllConsole', () => {
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) { 
+            vscode.window.showErrorMessage('Can\'t insert log because no document is open');
+            return; }
+
+        const document = editor.document;
+        const documentText = editor.document.getText();
+
+        let workspaceEdit = new vscode.WorkspaceEdit();
+
+        const logStatements = getAllLogStatements(document, documentText);
+
+        deleteFoundLogStatements(workspaceEdit, document.uri, logStatements);
+    });
+    context.subscriptions.push(deleteAllLogStatements);
+}
+exports.activate = activate;
+
+function deactivate() {
+}
+
+exports.deactivate = deactivate;
